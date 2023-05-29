d8d76e3fa3848e60d78c83f07f837de46c12e77b
eattato <80892781+eattato@users.noreply.github.com>
2023-05-29 23:02:45 +0900

    agent끼리 무한으로 뜨면서 각자 메모리에 가중치 저장함

--- training.js
+++ training.js
@@ -81,33 +81,46 @@ kkutu.connection
 .then(async () => {
     console.log("DB 로드됨");
 
-    let startWord = await kkutu.initWord();
-    let agents = [new Agent(), new Agent()];
-    console.log(`시작단어: '${startWord}'`);
-
-    agents.forEach((agent) => {
-        agent.updateCurrent(startWord);
-    });
-
-    let turn = 1;
+    // 경기 무한 반복
     while (true) {
-        turn = Number(!turn); // 0이면 1로, 1이면 0으로
-        let agent = agents[turn];
+        let startWord = await kkutu.initWord();
+        let agents = [new Agent(), new Agent()];
+        console.log(`시작단어: '${startWord}'`);
 
-        let word = await agent.getPick();
-        if (word != "gg") {
-            let wordStart = word.charAt(word.length - 1);
-            let alt = duum(wordStart);
-            let altText = wordStart != alt ? `(${alt})` : "";
+        agents.forEach((agent) => {
+            agent.updateCurrent(startWord);
+        });
 
-            console.log(`agent ${turn}: ${word}${altText}`);
-            agents.forEach((agent) => {
-                agent.updateCurrent(word);
-                agent.used.push(word);
-            });
-        } else {
-            console.log(`agent ${Number(!turn)}의 승리!`);
-            break;
+        let turn = 1;
+        while (true) { // 턴 진행
+            turn = Number(!turn); // 0이면 1로, 1이면 0으로
+            let agent = agents[turn];
+
+            let word = await agent.getPick();
+            if (word != "gg") {
+                let wordStart = word.charAt(word.length - 1);
+                let alt = duum(wordStart);
+                let altText = wordStart != alt ? `(${alt})` : "";
+
+                console.log(`agent ${turn}: ${word}${altText}`);
+                agents.forEach((agent) => {
+                    agent.updateCurrent(word);
+                    agent.used.push(word);
+                });
+            } else { // 게임 끝
+                console.log(`agent ${Number(!turn)}의 승리!`);
+
+                // 가중치 저장 & 초기화
+                let winnerUses = agents[Number(!turn)].used;
+                let loserUses = agent.used;
+                agents.forEach((agent) => {
+                    agent.updateWeight(winnerUses, 1);
+                    agent.updateWeight(loserUses, -1);
+                    agent.used = [];
+                });
+
+                break;
+            }
         }
     }
 
